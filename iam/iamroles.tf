2180dbdcfabef90ecd59c536a8c9799ae41ff0ba
darshan48 <tagarampriyadarshan@gmail.com>
2020-09-30 18:43:03 +0530

    first commit

---
+++ iam/iamroles.tf
@@ -0,0 +1,12 @@
+resource "aws_iam_role_policy" "lambda-policy" {
+  name = "test_policy"
+  role = aws_iam_role.lambda_role.id
+
+  policy = "${file("iam/lamdapolicy.json")}"
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+
+  assume_role_policy = "${file("iam/lambdarole.json")}"
+}
